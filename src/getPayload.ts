9c4c18fdca6bbc80f8e803633b317f7cc1b98772
Juosorioca420 <juosorioca@unal.edu.co>
2024-04-06 15:46:51 -0500

    Self Hosted Server Set-up (preparacion deploy)

---
+++ src/getPayload.ts
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv'
+import path from 'path'
+import type { InitOptions } from 'payload/config'
+import payload, { Payload } from 'payload'
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env')
+})
+
+// optimizacion de arranque
+let cached = (global as any).payload
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  }
+}
+
+// crear el database client y lo enviamos al cache
+interface Args { initOptions?: Partial<InitOptions> }
+  
+export const getPayloadClient = async ({ initOptions, }: Args = {}): Promise<Payload> => {
+
+    if (!process.env.PAYLOAD_SECRET) {
+      throw new Error('UwUsecret no identificado')
+    }
+    if (cached.client) { return cached.client }
+    
+    // guardar la promesa para no recargarla despues
+    if (!cached.promise) {
+      cached.promise = payload.init({
+        // aca irian las credenciales del correo tambien
+
+        secret: process.env.PAYLOAD_SECRET,
+        local: initOptions?.express ? false : true,
+        
+        // y todo lo demas que pongamos en las opciones de configuracion inicial
+        ...(initOptions || {}), 
+      })
+    }
+  
+    try {
+      cached.client = await cached.promise
+    } 
+    catch (e: unknown) {
+      cached.promise = null
+      throw e
+    }
+  
+    return cached.client
+}
