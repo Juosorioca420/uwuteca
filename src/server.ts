64cf9300f5d38f50c969a0af248134d954ba3208
Juosorioca420 <juosorioca@unal.edu.co>
2024-04-11 00:55:17 -0500

    to deploy fixes

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ import * as trpcExpress from '@trpc/server/adapters/express'
 import { appRouter } from './trpc'
 import { inferAsyncReturnType } from '@trpc/server'
 
+import nextBuild from 'next/dist/build'
+import path from 'path'
+
+
 const app = express()
 const PORT = Number(process.env.PORT) || 3000 // puerto local 3000 para desarrollo
 
@@ -16,6 +20,7 @@ const createContext = ( {req, res} : trpcExpress.CreateExpressContextOptions ) =
 export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
 const start = async () => {
+
     const payload = await getPayloadClient({
         initOptions: {
             express: app,
@@ -30,6 +35,19 @@ const start = async () => {
             createContext,
         })
     )
+
+    if (process.env.NEXT_BUILD) {
+        app.listen(PORT, async () => {
+          payload.logger.info( 'Next.js is building for production' )
+    
+          // @ts-expect-error
+          await nextBuild(path.join(__dirname, '../'))
+    
+          process.exit()
+        })
+    
+        return
+      }
 
     app.use( (req, res) => nextHandler(req, res) )
 
